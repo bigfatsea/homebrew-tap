21fdb3cd3643e35fca78119d04dfb6e144a1b8d9
Stanford <bigfatsea@gmail.com>
2026-09-12 23:32:27 +0800

    vmr 0.7.0

--- Formula/vmr.rb
+++ Formula/vmr.rb
@@ -1,28 +1,28 @@
-# Ver 2026-08-29 17:27, by gemini-3.7-flash
+# Ver 2026-09-12 23:25, by Stanford
 
 class Vmr < Formula
   desc "Local-first, single-binary LLM router for AI agents (byte-faithful passthrough, failover, flight-recorder audit log)"
   homepage "https://github.com/bigfatsea/vmr"
-  version "0.6.3"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bigfatsea/vmr/releases/download/v0.6.3/vmr_darwin_arm64.tar.gz"
-      sha256 "e93a218f3063157137b882fc8eacce1aa7ec5ba5a2058948d948d846a9d93a27"
+      url "https://github.com/bigfatsea/vmr/releases/download/v0.7.0/vmr_darwin_arm64.tar.gz"
+      sha256 "cb55a495fec8cb58af05ad7988ce0e42f504714230b8978ae8d95c4e478ea0c0"
     else
-      url "https://github.com/bigfatsea/vmr/releases/download/v0.6.3/vmr_darwin_amd64.tar.gz"
-      sha256 "1e733aed287e4d01278a8e98d5ca01693f8da35076cd75c2de32e46a32b52b63"
+      url "https://github.com/bigfatsea/vmr/releases/download/v0.7.0/vmr_darwin_amd64.tar.gz"
+      sha256 "658f10f3b7c09f5c7f8a6f3f6ca66c3bd63e072fb24329b57af046a2ba420d07"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bigfatsea/vmr/releases/download/v0.6.3/vmr_linux_arm64.tar.gz"
-      sha256 "0c6593cc425b0d81c0b7f6e09dc0938edee9ab4a5f5623e947f911a202eda8cf"
+      url "https://github.com/bigfatsea/vmr/releases/download/v0.7.0/vmr_linux_arm64.tar.gz"
+      sha256 "45c41c1856794f3fa49679b1df2bb3dee3768d5f064fb87a151061d45c26e080"
     else
-      url "https://github.com/bigfatsea/vmr/releases/download/v0.6.3/vmr_linux_amd64.tar.gz"
-      sha256 "ea50e4ed8c2f9436812916089211f6d134e65bbb423ccc944f8801cecdf2dbee"
+      url "https://github.com/bigfatsea/vmr/releases/download/v0.7.0/vmr_linux_amd64.tar.gz"
+      sha256 "fb8ffc89a898ba5ae9979b04b9cc3e89b9d689741446368532145106b5e7eee5"
     end
   end
 
